d306d822316730b232bc642dc0b4c692f1b4df59
Dmitry <09.09.2005@inbox.ru>
2023-08-15 08:48:43 +0400

    made an intro

--- src/components/intro/Intro.jsx
+++ src/components/intro/Intro.jsx
@@ -1,9 +1,37 @@
 import "./intro.scss";
-
+import { ArrowDownward } from "@mui/icons-material";
+import { init } from "ityped";
+import { useEffect, useRef } from "react";
 export default function Intro() {
+  const textRef = useRef();
+
+  useEffect(() => {
+    init(textRef.current, {
+      showCursor: true,
+      backDelay: 1500,
+      backSpeed: 60,
+      strings: ["Frontend Developer"],
+    });
+  }, []);
   return (
     <div className="intro" id="intro">
-      Intro
+      <div className="left">
+        <div className="imgContainer">
+          <img src="assets/me.png" alt="Me" className="" />
+        </div>
+      </div>
+      <div className="right">
+        <div className="wrapper">
+          <h2>Hi There, I'm</h2>
+          <h1>Dima Boltach</h1>
+          <h3>
+            <span ref={textRef}></span>
+          </h3>
+        </div>
+        <a href="#portfolio">
+          <ArrowDownward />
+        </a>
+      </div>
     </div>
   );
 }
